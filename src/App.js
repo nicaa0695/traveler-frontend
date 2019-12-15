ff0a4ddd4523b5a9015590ec6d00f34995151d2b
nicaa0695 <nicaa0695@gmail.com>
2019-12-15 11:30:04 -0700

    installed react, react-router-do, redux, redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Destinations from './containers/Destinations';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className='App'>
-        
+        <NavBar />
+        <Route exact path='/' component={ Home } />
+        <Destinations />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import destinationReducer from './reducers/destinationReducer';
 import App from './App';
+import './App.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store = { store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
 
