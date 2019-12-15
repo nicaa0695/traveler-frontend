58f6d137c1aa85970f84b4c4a0b053dae90bfe85
nicaa0695 <nicaa0695@gmail.com>
2019-12-14 17:31:11 -0700

    added destinationlist.js

---
+++ src/components/DestinationList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+
+const DestinationsList = props => {
+
+  return (
+    <div className='DestinationContainer'>
+
+      <div>
+        <Link to='/destinations/new' className='NewDestinationButton' role='button'>Add A New Destination</Link>
+      </div>
+
+      <h3>Destinations</h3>
+      {props.destinations.map(destination =>
+        <Link key={destination.id} to={`/destinations/${destination.id}`}>
+          <Card className = 'DestinationCard'>
+            <CardBody>
+              <CardTitle>{destination.name} - {destination.country}</CardTitle>
+              <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+            </CardBody>
+          </Card>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default DestinationsList;
