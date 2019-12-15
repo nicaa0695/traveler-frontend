2ad49fbdd678b3a27daa64079536b29b7c81a2a5
nicaa0695 <nicaa0695@gmail.com>
2019-12-15 14:28:20 -0700

    modified destinations container. added paths

--- src/containers/Destinations.js
+++ src/containers/Destinations.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { fetchDestinations } from '../actions/destinationActions';
+import DestinationInput from '../components/DestinationInput';
+import DestinationsList from '../components/DestinationsList';
+import DestinationShow from '../components/DestinationShow';
+import VisitedDestinations from '../components/VisitedDestinations';
+import BucketList from '../components/BucketList';
 
 class Destinations extends React.Component {
     render() {
         return (
           <div>
-
+            <Switch>
+              <Route path='/destinations/new' component={DestinationInput} />
+              <Route path='/destinations/visited' render={(routerProps) => <VisitedDestinations {...routerProps} destinations={this.props.destinations} />} />
+              <Route path='/destinations/bucket-list' render={(routerProps) => <BucketList {...routerProps} destinations={this.props.destinations} />}  />
+              <Route path='/destinations/:id' render={(routerProps) => <DestinationShow {...routerProps} destinations={this.props.destinations} />} />
+              <Route path='/destinations' render={(routerProps) => <DestinationsList {...routerProps} destinations={this.props.destinations} />} />
+            </Switch>
           </div>
         );
     }
