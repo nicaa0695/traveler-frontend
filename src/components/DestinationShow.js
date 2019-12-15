2fa1a52b0bc9c51135afa22188b7901000519b16
nicaa0695 <nicaa0695@gmail.com>
2019-12-15 14:25:02 -0700

    added destinationShow

---
+++ src/components/DestinationShow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Card, CardBody, CardTitle, CardImg, Button } from 'reactstrap';
+import Categories from '../containers/Categories';
+import { deleteDestination, toggleVisited, toggleBucketList } from '../actions/destinationActions';
+
+
+const DestinationShow = props => {
+
+  let destination = props.destinations.filter(destination => destination.id == props.match.params.id)[0];
+
+  const handleDelete = () => {
+    props.deleteDestination(destination.id);
+    const path = `/destinations`;
+    props.history.push(path);
+  };
+
+  const handleVisited = () => {
+    props.toggleVisited(destination, destination.id)
+  }
+
+  const handleBucketList = () => {
+    props.toggleBucketList(destination, destination.id)
+  }
+
+  return (
+    <div>
+      <Card className = 'DestinationShow'>
+        <CardBody>
+          <Button onClick={handleDelete} className='DeleteButton'>x</Button>
+          <CardTitle>{destination && destination.name} - {destination && destination.country}</CardTitle>
+          <CardImg className='DestinationImage' src={destination && destination.image} alt={destination && destination.name} />
+        </CardBody>
+        <Button onClick={handleVisited} className='VisitedButton'>
+          {destination && destination.visited === false ? 'Add to Visited' : 'Visited'}
+        </Button>
+        <Button onClick={handleBucketList} className='BucketListButton'>
+          {destination && destination.bucket_list === false ? 'Add to Bucket List' : 'In your Bucket List'}
+        </Button>
+      </Card>
+
+      <Categories destination={destination} />
+    </div>
+  );
+};
+
+export default connect(null, { deleteDestination, toggleVisited, toggleBucketList })(DestinationShow);
