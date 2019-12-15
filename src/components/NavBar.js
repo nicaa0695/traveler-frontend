48953db4dc413ae5941c0d95b3d0a4e64a856743
nicaa0695 <nicaa0695@gmail.com>
2019-12-14 17:25:09 -0700

    added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+
+  return (
+    <div className='NavbarContainer'>
+      <NavLink to='/' className='NavbarLink'>Home</NavLink>
+      <NavLink to='/destinations' className='NavbarLink'>Destinations</NavLink>
+      <NavLink to='/destinations/visited' className='NavbarLink'>Visited Destinations</NavLink>
+      <NavLink to='/destinations/bucket-list' className='NavbarLink'>Bucket List</NavLink>
+    </div>
+  );
+};
+
+export default NavBar;
