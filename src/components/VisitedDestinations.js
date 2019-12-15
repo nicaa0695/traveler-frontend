8d1a713249d58e33572a0c98e222c2bfe3fd20a5
nicaa0695 <nicaa0695@gmail.com>
2019-12-14 17:27:14 -0700

    added visiteddestinations.js

---
+++ src/components/VisitedDestinations.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+
+const VisitedDestinations = props => {
+
+  return (
+    <div className='DestinationContainer'>
+      <h3>Places you have visited</h3>
+
+      {props.destinations.map(destination => {
+        if (destination.visited === true) {
+          return (
+            <Link key={destination.id} to={`/destinations/${destination.id}`}>
+              <Card className = 'DestinationCard'>
+                <CardBody>
+                  <CardTitle>{destination.name} - {destination.country}</CardTitle>
+                  <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+                </CardBody>
+              </Card>
+            </Link>
+          )
+        } else {
+          return (
+            null
+          )
+        }
+      })}
+    </div>
+  );
+};
+
+export default VisitedDestinations;
