d67c8cf94116242e949a3c085b8dde96a19a73bb
nicaa0695 <nicaa0695@gmail.com>
2019-12-15 14:59:29 -0700

    modified activities container

--- src/containers/Acivities.js
+++ src/containers/Acivities.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ActivityInput from '../components/ActivityInput';
+import ActivitiesList from '../components/ActivitiesList';
 
 class Activities extends React.Component {
 
     render() {
       return (
         <div>
-          
+          <ActivityInput destination={this.props.destination} />
+          <ActivitiesList activities={this.props.destination && this.props.destination.activities} />
         </div>
       );
     }
