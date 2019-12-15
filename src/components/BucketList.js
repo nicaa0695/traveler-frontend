8728c4eb24b9b64ae02b8e755974c58ecdcadbe0
nicaa0695 <nicaa0695@gmail.com>
2019-12-15 11:05:15 -0700

    added bucketlist component

---
+++ src/components/BucketList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+
+const BucketList = props => {
+
+  return (
+    <div className='DestinationContainer'>
+      <h3>Destinations in your Bucket List</h3>
+
+      {props.destinations.map(destination => {
+        if (destination.bucket_list === true) {
+          return (
+            <Link key={destination.id} to={`/destinations/${destination.id}`}>
+              <Card key={destination.id} className = 'DestinationCard'>
+                <CardBody>
+                  <CardTitle>{destination.name} - {destination.country}</CardTitle>
+                  <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+                </CardBody>
+              </Card>
+            </Link>
+          )
+        } else {
+          return (
+            null
+          )
+        }
+      })}
+    </div>
+  );
+};
+
+export default BucketList;
