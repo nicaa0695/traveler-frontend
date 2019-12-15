545ab53aa6776f22748d9a85d418aeb1697bbd70
nicaa0695 <nicaa0695@gmail.com>
2019-12-14 17:08:27 -0700

    added home page components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <div className='HomePage'>
+      <h3>Travelers</h3>
+      <p>Where do want to go?</p>
+    </div>
+  );
+};
+
+export default Home;
