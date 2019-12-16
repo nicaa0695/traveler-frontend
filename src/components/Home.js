0fd51e9c5e0079b8de2ecc1d6d12d2096064d39f
nicaa0695 <nicaa0695@gmail.com>
2019-12-15 17:02:24 -0700

    adding styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className='HomePage'>
       <h3>Travelers</h3>
       <p>Where do want to go?</p>
-      <img className='HomePageImage' src='105701227-6ED1-REQ-AirbusBoeing-012519.jpg' alt='traveling' />
+      <img className='HomePageImage' src='https://desktopwalls.net/wp-content/uploads/2014/08/Aircraft%20Wing%20Blue%20Sunset%20Sky%20Clouds%20Desktop%20Wallpaper.jpg' alt='traveling' />
     </div>
   );
 };
